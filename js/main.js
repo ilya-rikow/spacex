46f1ae3d9d99d6e76639ccb6dda15163fe0e5e7a
ilya-rikow <62751670+ilya-rikow@users.noreply.github.com>
2020-08-08 20:27:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var instance = new vidbg('.video', {
-    mp4: 'video/world.mp4', // URL or relative path to MP4 video
+    mp4: 'video/world.mp', // URL or relative path to MP4 video
     webm: 'video/world.webm', // URL or relative path to webm video
     poster: 'video/poster.jpg', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
